3574cf348f8ed76bbd10dd5f7fe00e4b6e4b33f3
Gorbachev Sergey <sergei260995@gmail.com>
2023-04-06 13:42:37 +0200

    add liquibase

---
+++ liquibase-4.20.0/db/changelog/dml.sql
@@ -0,0 +1,16 @@
+insert into person (full_name,year_of_birth) values ('Скимова М.В.', 1990);
+insert into person (full_name,year_of_birth) values ('Симачева В.А.', 1945);
+insert into person (full_name,year_of_birth) values ('Раприна Е.А.', 1950);
+insert into person (full_name,year_of_birth) values ('Менделеева А.Ю.', 1954);
+insert into person (full_name,year_of_birth) values ('Кожеч М.М.', 1923);
+insert into person (full_name,year_of_birth) values ('Сергеева А.Н.', 1966);
+insert into person (full_name,year_of_birth) values ('Герасимова Ж.Н.', 1949);
+
+insert into book ("name",autor,year_of_printing) values ('День опричника','Владимир Сорокин',2006);
+insert into book ("name",autor,year_of_printing) values ('Над пропастью во ржи','Джером Сэлинджер', 1951);
+insert into book ("name",autor,year_of_printing) values ('Тайные виды на гору Фудзи','Владимир Пелевин',2018);
+insert into book ("name",autor,year_of_printing) values ('Игра в бисер','Герман Гессе',1943);
+insert into book ("name",autor,year_of_printing) values ('Психопатология обыденной жизни','Фрейд Зигмунд',1904);
+insert into book ("name",autor,year_of_printing) values ('Бытие и время','Мартин Хайдеггер',1927);
+insert into book ("name",autor,year_of_printing) values ('Психология','Ивченко К.А.',2010);
+insert into book ("name",autor,year_of_printing) values ('Философия Java ','Брюс Эккель',2018);
